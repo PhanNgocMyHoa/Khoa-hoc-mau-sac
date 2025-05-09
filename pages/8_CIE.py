45281e4c512f0879b2c8ef45228a33bdba9e1f1c
PhanNgocMyHoa <myyhoa.2837@gmail.com>
2025-05-09 15:36:41 +0700

    Add files via upload

--- pages/8_CIE.py
+++ pages/8_CIE.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from colour import sRGB_to_XYZ, XYZ_to_Lab
 
 st.set_page_config(layout="wide")
-st.title("Dải màu quang phổ (400nm - 700nm) và thông tin màu")
+st.title("Dải màu quang phổ (400nm - 700nm)")
 
-# Hàm chuyển bước sóng (nm) sang RGB
+# Hàm chuyển bước sóng (nm) sang RGB (đơn giản hóa)
 def wavelength_to_rgb(wavelength):
     gamma = 0.8
+    intensity_max = 1
+
     if 380 <= wavelength <= 440:
         attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
         R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
@@ -30,21 +31,21 @@ def wavelength_to_rgb(wavelength):
         R = 1.0
         G = (-(wavelength - 645) / (645 - 580)) ** gamma
         B = 0.0
-    elif 645 < wavelength <= 700:
-        attenuation = 0.3 + 0.7 * (700 - wavelength) / (700 - 645)
+    elif 645 < wavelength <= 780:
+        attenuation = 0.3 + 0.7 * (780 - wavelength) / (780 - 645)
         R = (1.0 * attenuation) ** gamma
         G = 0.0
         B = 0.0
     else:
         R = G = B = 0.0
 
-    return int(R * 255), int(G * 255), int(B * 255)
+    return (int(R * 255), int(G * 255), int(B * 255))
 
-# Tạo phổ màu
+# Tạo band màu
 wavelengths = np.arange(400, 701, 1)
 colors = [wavelength_to_rgb(wl) for wl in wavelengths]
 
-# Hiển thị dải màu
+# Vẽ dải màu
 fig, ax = plt.subplots(figsize=(10, 1))
 for i, color in enumerate(colors):
     ax.axvspan(i, i + 1, color=np.array(color) / 255)
@@ -54,19 +55,5 @@ ax.set_xticks(np.linspace(0, len(wavelengths), 7))
 ax.set_xticklabels(np.linspace(400, 700, 7, dtype=int))
 ax.set_yticks([])
 ax.set_title("Phổ ánh sáng khả kiến (400 - 700nm)")
+
 st.pyplot(fig)
-
-# Lựa chọn bước sóng
-selected_wavelength = st.slider("Chọn bước sóng (nm):", 400, 700, 550)
-
-# Tính màu
-r, g, b = wavelength_to_rgb(selected_wavelength)
-rgb_norm = np.array([r, g, b]) / 255.0
-xyz = sRGB_to_XYZ(rgb_norm)
-lab = XYZ_to_Lab(xyz)
-
-# Hiển thị màu và thông tin
-st.markdown(f"### Thông tin tại {selected_wavelength} nm")
-st.color_picker("Màu tương ứng", f'#{r:02X}{g:02X}{b:02X}', disabled=True)
-st.write(f"**RGB:** R={r}, G={g}, B={b}")
-st.write(f"**Lab:** L={lab[0]:.2f}, a={lab[1]:.2f}, b={lab[2]:.2f}")
